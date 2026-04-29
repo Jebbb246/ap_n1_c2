4e000bbb864fab85eaec2a8d162c6d4f1804af76
Jebbb246 <riquelmeagustin246@gmail.com>
2026-04-29 09:49:13 -0400

    29/04

--- funciones/crear_funciones.py
+++ funciones/crear_funciones.py
@@ -1,40 +1,88 @@
 # Dentro del lenguaje, tenemos la opción de crear nuestras PROPIAS funciones
 # para eso usamos la palabra reservada DEF
 
+# DEF es acrínimo de DEFINIR, porque estamos definiendo una FUNCIÓN
+# Luego de DEF ponemos el NOMBRE de la función
+# Luego del NOMBRE de la FUNCIÓN ponemos sus ARGUMENTOS
+# Los ARGUMENTOS serán los insumos que usará la función para hacer su trabajo
+
 def suma(a,b):
     # Este será el contenido de la función
     resultado = a + b
-    print(resultado)
+    return resultado
 
 def resta(a,b):
     resultado = a - b
-    print(resultado)
+    return resultado
 
 def multiplicacion(a,b):
     resultado = a * b
-    print(resultado)
+    return resultado
 
 def division(a,b):
-    if b == 0:
-        print('No se puede dividir por 0')
-    else: 
-        resultado = a/b
-        print(resultado)
+    if b != 0:
+        resultado = a / b
+        return resultado
+    else:
+        print('No es posible dividir por 0!')
 
 def pedir_datos():
-    numero_1 = input('Ingrese primer número: ')
-    numero_2 = input('Inrese segundo número: ')
-    return(numero_1, numero_2)
+    str_numero_1 = input('Ingrese primer número: ')
+    str_numero_2 = input('Inrese segundo número: ')
 
-def validar_numeros(num_1,num_2):
-    if num_1.isdigit() and num_2.isdigit():
-        num_1 = float(num_1)
-        num_2 = float(num_2)
+    num_1 = convertir_float(str_numero_1)
+    num_2 = convertir_float(str_numero_2)
+
+    if num_1 and num_2 != False:
+        return(num_1,num_2)
     else:
-        print('Valor NO corresponde a un número')
-    return
+        return(False,False)
 
-    suma(num_1,num_2)
-    resta(num_1,num_2)
-    multiplicacion(num_1,num_2)
-    division(num_1,num_2)
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except (ValueError, TypeError):
+        return False
+
+
+print('\nBienvenido a Mi primera CALCULADORA')
+print('====================================')
+ciclo = True
+
+while ciclo == True:
+    print('\n[1] Suma')
+    print('[2] Resta')
+    print('[3] Multiplicación')
+    print('[4] División')
+    print('[0] Salir')
+    opcion = input('\nSeleccione su operación [0-4]: ')
+
+    opciones_validas = ['0','1','2','3','4']
+
+    if opcion in(opciones_validas):
+        if opcion == '0':
+            ciclo = False
+            print('Gracias por usar mi calculadora!')
+            print('Saliendo...')
+        else:
+            a,b = pedir_datos()
+            operacion = ''
+
+            if a and b != False:
+                if opcion == '1':
+                    operacion = '+'
+                    valor = suma(a,b)
+                elif opcion == '2':
+                    operacion = '-'
+                    valor = resta(a,b)
+                elif opcion == '3':
+                    operacion = 'x'
+                    valor = multiplicacion(a,b)
+                elif opcion == '4':
+                    operacion = '/'
+                    valor = division(a,b)
+                print(f'{a} {operacion} {b} = {valor}')
+            else:
+                print('Valor NO corresponde a un número!')
+    else:
+        print('Opción NO válida.')
